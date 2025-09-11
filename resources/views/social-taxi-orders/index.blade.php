9a9377776667702bc96bf1fdf67b420946a47798
semen <semen@semenov>
2025-09-11 16:43:26 +0400

    Предварительный расчет Signed-off-by:semen <semen@semenov>

--- resources/views/social-taxi-orders/index.blade.php
+++ resources/views/social-taxi-orders/index.blade.php
@@ -2,17 +2,17 @@
     <div class="bg-gray-100 py-6">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <!-- Заголовок и кнопки -->
-            @include('social-taxi-orders.components.header')
+            @include('social-taxi-orders.index_components.header')
 
             <!-- Фильтры -->
-            @include('social-taxi-orders.components.filters')
+            @include('social-taxi-orders.index_components.filters')
             <!-- Пагинация -->
             <div class="mt-4 mb-2">
                 {{ $orders->appends(request()->all())->links() }}
             </div>
 
             <!-- Таблица с сортировкой -->
-            @include('social-taxi-orders.components.table')
+            @include('social-taxi-orders.index_components.table')
 
             <!-- Пагинация -->
             <div class="mt-4">
