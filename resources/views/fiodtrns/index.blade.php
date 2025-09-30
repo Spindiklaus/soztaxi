adcc9c1e11c4866fc7b164d83e5805bea67de3ca
semen <semen@semenov>
2025-09-30 17:14:49 +0400

    Доработка кнопок в такси Signed-off-by:semen <semen@semenov>

--- resources/views/fiodtrns/index.blade.php
+++ resources/views/fiodtrns/index.blade.php
@@ -171,10 +171,6 @@
                             </th>
                             <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-bold uppercase tracking-wider">
-                                Комментарии
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-bold uppercase tracking-wider">
                                 Оператор
                             </th>
                             <th scope="col"
@@ -187,8 +183,12 @@
                         <template x-for="fiodtrn in filteredFioDtrns" :key="fiodtrn.id">
                             <tr>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" 
-                                    x-text="fiodtrn.fio"
                                     :class="fiodtrn.rip_at ? 'bg-gray-500' : ''">
+                                    <div x-text="fiodtrn.fio"></div>
+                                    <div x-show="fiodtrn.komment" 
+                                         class="text-xs text-gray-600 mt-1" 
+                                         x-text="fiodtrn.komment">
+                                    </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" x-text="fiodtrn.kl_id"></td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" x-text="fiodtrn.data_r"></td>
@@ -198,7 +198,6 @@
                                     </span>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" x-text="fiodtrn.rip_at"></td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" x-text="fiodtrn.komment"></td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" x-text="fiodtrn.operator"></td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm space-x-2 flex justify-end">
                                     <a :href="`/fiodtrns/${fiodtrn.id}?sort=${sortField}&direction=${sortDirection}`"
