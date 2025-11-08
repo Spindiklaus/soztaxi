1837aa5109dc058e19bc0105f546984c255f3dd1
Home <Home@DESKTOP-28IL130.mshome.net>
2025-11-09 00:43:05 +0400

    увеличил ширину модальных окон

--- resources/views/social-taxi-orders/show-components/client-trips-modal.blade.php
+++ resources/views/social-taxi-orders/show-components/client-trips-modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Модальное окно с поездками клиента -->
 <div id="client-trips-modal" class="fixed inset-0 bg-black bg-opacity-50 hidden items-center justify-center z-50 p-4">
-        <div class="bg-white rounded-lg w-full max-w-4xl min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col mx-auto">
+        <div class="bg-white rounded-lg w-full max-w-6xl min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col mx-auto">
             <div class="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
                 <h3 id="modal-title" class="text-lg font-semibold text-gray-800">Заказы</h3>
                 <button onclick="closeClientTripsModal()" class="text-gray-400 hover:text-gray-600">
