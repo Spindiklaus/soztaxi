035d4ab5ffc4b6c2f2059c14fda558a21617d71e
semen <semen@semenov>
2025-11-24 11:18:20 +0400

    доработка таблицы клиентов Signed-off-by:semen <semen@semenov>

---
+++ database/migrations/2025_11_24_110626_add_client_id_foreign_key_to_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreign('client_id')
+                  ->references('id')
+                  ->on('fio_dtrns')
+                  ->onDelete('restrict')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+        });
+    }
+};
